64974d55d1b5fb38bccb7b138cb355435691e852
Christian Alamillo <calam002@ucr.edu>
2021-06-13 00:21:25 -0700

    Added some more some restructuring for the html user and computer choice labels

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,30 @@
 
 //this function will randomly select rock, paper, or scissors from an array
 function computerPlay() {
-    const rpm = ['Rock', 'Paper', 'Scissors'];
+    const rps = ['Rock', 'Paper', 'Scissors'];
     let index = Math.floor(Math.random() * 3);
-    return rpm[index];
+    return rps[index];
+}
+
+//this function verifies that the user inputs rock, paper, or scissors
+function userInputValid(validChoice) {
+    validChoice = prompt('Rock, Paper, or Scissors?').toUpperCase(); //allows user response to be case-insensitive
+    while (validChoice != 'ROCK' && validChoice != 'PAPER' && validChoice != 'SCISSORS') {
+        validChoice = prompt('That\'s not an option. Choose again.').toUpperCase();
+    }
+    return validChoice;
 }
 
 //this function will take user input and computer choice as arguments and generate 
 //win or lose messages based on results
-function roundRPM(playerSelection, computerSelection) {
+function roundRPS(playerSelection, computerSelection) {
     if (playerSelection === 'ROCK') {
+        document.getElementById('userChoice').innerHTML = 'Your choice: Rock';
         if (computerSelection === 'Scissors') {
-            document.querySelector('p').innerHTML = 'You win!';
+            document.querySelector('p').innerHTML = 'You win! Rock beats scissors!';
         }
         else if (computerSelection === 'Paper') {
-            document.querySelector('p').innerHTML = 'You lose!';
+            document.querySelector('p').innerHTML = 'You lose. Paper beats rock...';
         }
         else {
             document.querySelector('p').innerHTML = 'It\'s a tie!';
@@ -22,11 +32,12 @@ function roundRPM(playerSelection, computerSelection) {
     }
 
     else if (playerSelection === 'SCISSORS') {
+        document.getElementById('userChoice').innerHTML = 'Your choice: Scissors';
         if (computerSelection === 'Paper') {
-            document.querySelector('p').innerHTML = 'You win!';
+            document.querySelector('p').innerHTML = 'You win! Scissors beats paper!';
         }
         else if (computerSelection === 'Rock') {
-            document.querySelector('p').innerHTML = 'You lose!';
+            document.querySelector('p').innerHTML = 'You lose. Rock beats scissors...';
         }
         else {
             document.querySelector('p').innerHTML = 'It\'s a tie!';
@@ -34,25 +45,26 @@ function roundRPM(playerSelection, computerSelection) {
     }
 
     else if (playerSelection === 'PAPER') {
+        document.getElementById('userChoice').innerHTML = 'Your choice: Paper';
         if (computerSelection === 'Rock') {
-            document.querySelector('p').innerHTML = 'You win!';
+            document.querySelector('p').innerHTML = 'You win! Paper beats rock!';
         }
         else if (computerSelection === 'Scissors') {
-            document.querySelector('p').innerHTML = 'You lose!';
+            document.querySelector('p').innerHTML = 'You lose. Scissors beats paper...';
         }
         else {
             document.querySelector('p').innerHTML = 'It\'s a tie!';
         }
     }
-
+    
 }
 
 
 // initialize variable to be assigned computer value
 let computerChoice = computerPlay();
-// ask user for choice
-let userChoice = prompt('Rock, Paper, or Scissors?').toUpperCase();
-roundRPM(userChoice, computerChoice);
+let userChoice = userInputValid();
+
+roundRPS(userChoice, computerChoice);
 
 console.log('Computer: ' + computerChoice);
 console.log('You: ' + userChoice);
