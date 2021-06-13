75f3cbcee0e056f85a5a10e62b1ebdb66c238559
Christian Alamillo <calam002@ucr.edu>
2021-06-12 18:39:05 -0700

    changed user input to take in upper case characters

--- scripts/main.js
+++ scripts/main.js
@@ -6,5 +6,53 @@ function computerPlay() {
     return rpm[index];
 }
 
+//this function will take user input and computer choice as arguments and generate 
+//win or lose messages based on results
+function roundRPM(playerSelection, computerSelection) {
+    if (playerSelection === 'ROCK') {
+        if (computerSelection === 'Scissors') {
+            document.querySelector('p').innerHTML = 'You win!';
+        }
+        else if (computerSelection === 'Paper') {
+            document.querySelector('p').innerHTML = 'You lose!';
+        }
+        else {
+            document.querySelector('p').innerHTML = 'It\'s a tie!';
+        }
+    }
+
+    else if (playerSelection === 'SCISSORS') {
+        if (computerSelection === 'Paper') {
+            document.querySelector('p').innerHTML = 'You win!';
+        }
+        else if (computerSelection === 'Rock') {
+            document.querySelector('p').innerHTML = 'You lose!';
+        }
+        else {
+            document.querySelector('p').innerHTML = 'It\'s a tie!';
+        }
+    }
+
+    else if (playerSelection === 'PAPER') {
+        if (computerSelection === 'Rock') {
+            document.querySelector('p').innerHTML = 'You win!';
+        }
+        else if (computerSelection === 'Scissors') {
+            document.querySelector('p').innerHTML = 'You lose!';
+        }
+        else {
+            document.querySelector('p').innerHTML = 'It\'s a tie!';
+        }
+    }
+
+}
+
+
+// initialize variable to be assigned computer value
 let computerChoice = computerPlay();
-console.log(computerChoice);
+// ask user for choice
+let userChoice = prompt('Rock, Paper, or Scissors?').toUpperCase();
+roundRPM(userChoice, computerChoice);
+
+console.log('Computer: ' + computerChoice);
+console.log('You: ' + userChoice);
